368bed91701ab7a02e3a70ce3996eb164474fb02
coop0183 <coop0183@gmail.com>
2025-08-17 12:26:00 -0500

    Initial commit 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/App/App.jsx 	modified:   src/main.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,7 @@
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
 import "./App.css";
 import Header from "../Header/Header";
 
@@ -5,6 +9,15 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+      <Routes>
+        <Route path="/reviews" element={<Reviews />} />
+      </Routes>
+      <Routes>
+        <Route path="/aboutme" element={<AboutMe />} />
+      </Routes>
     </div>
   );
 }
