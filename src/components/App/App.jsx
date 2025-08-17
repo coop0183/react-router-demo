2aa46799662666c8e8df41028249284bf45d483a
coop0183 <coop0183@gmail.com>
2025-08-17 15:31:06 -0500

    Commit: Update App and AboutUs components to include nested routes 	new file:   src/components/AboutMe/AboutContent.css 	new file:   src/components/AboutMe/AboutMe.css 	modified:   src/components/AboutMe/AboutMe.jsx 	new file:   src/components/AboutMe/Contact.jsx 	new file:   src/components/AboutMe/Hobbies.jsx 	new file:   src/components/AboutMe/MyStory.jsx 	new file:   src/components/AboutUs/AboutContent.css 	renamed:    src/components/AboutUs/AboutUs/AboutUs.css -> src/components/AboutUs/AboutUs.css 	new file:   src/components/AboutUs/AboutUs.jsx 	deleted:    src/components/AboutUs/AboutUs/AboutUs.jsx 	new file:   src/components/AboutUs/SiteHistory.jsx 	new file:   src/components/AboutUs/SiteMission.jsx 	modified:   src/components/App/App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "../Dashboard/Dashboard";
 import Reviews from "../Reviews/Reviews";
 import AboutMe from "../AboutMe/AboutMe";
-import AboutUs from "../AboutUs/AboutUs/AboutUs";
+import Contact from "../AboutMe/Contact";
+import Hobbies from "../AboutMe/Hobbies";
+import MyStory from "../AboutMe/MyStory";
+import AboutUs from "../AboutUs/AboutUs";
+import SiteHistory from "../AboutUs/SiteHistory";
+import SiteMission from "../AboutUs/SiteMission";
 import "./App.css";
 import Header from "../Header/Header";
 
@@ -13,8 +18,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews />} />
-        <Route path="/about-me" element={<AboutMe />} />
-        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
+        <Route path="/about-us" element={<AboutUs />}>
+          <Route path="site-history" element={<SiteHistory />} />
+          <Route path="site-mission" element={<SiteMission />} />
+        </Route>
       </Routes>
     </div>
   );
